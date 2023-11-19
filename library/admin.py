4db47717591cdfeb3bccab0a0ffbda4b7d17d97f
Deadly mind <oussamaayari2014@gmail.com>
2023-11-19 05:22:15 +0100

    define models then register the models in admin

--- library/admin.py
+++ library/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Author, Book, Review
+
+
 
 # Register your models here.
+
+admin.site.register(Author)
+admin.site.register(Book)
+admin.site.register(Review)
+
+
+
--- library/models.py
+++ library/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
 # Create your models here.
+
+"""
+Author:
+	name
+	birth_date
+	biography
+Book:
+	title
+	autor
+	publication_date
+	price
+Review:
+    book 
+    reviewer_name
+    content 
+    rating(1:5)  
+----------------------------  
+ToDo:
+    - github repo with project 
+   	- design models 
+    - Generate CRUD for Books
+
+"""
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    birth_date = models.DateField()
+    biography = models.TextField()
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    publication_date = models.DateField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+class Review(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    reviewer_name = models.CharField(max_length=100)
+    content = models.TextField()
+    rating = models.IntegerField(choices=[(i, i) for i in range(1,6)])
+
+
+
