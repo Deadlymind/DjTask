d0a0370375613369f803017803655b562be644d3
Deadly mind <oussamaayari2014@gmail.com>
2023-11-19 08:55:59 +0100

    add admin customization

--- library/admin.py
+++ library/admin.py
@@ -5,9 +5,17 @@ from .models import Author, Book, Review
 
 # Register your models here.
 
-admin.site.register(Author)
-admin.site.register(Book)
-admin.site.register(Review)
+class ProductAdmin(admin.ModelAdmin):       # Custom admin class for the Product model
+    list_display = ['name', 'birth_date']   # Specifies the fields to display in the list view.
+    list_filter = ['name']                  # Specifies the fields to provide filtering options for.
+    search_fields = ['name']                # Enables search functionality on the 'name' field
+
+
+
+
+admin.site.register(Author, ProductAdmin) # Registers the Author model with the Django admin site for management.
+admin.site.register(Book)                 # Registers the Book model with the Django admin site for management.
+admin.site.register(Review)               # Registers the Review model with the Django admin site for management.
 
 
 
