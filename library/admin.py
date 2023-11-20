448b1bb54eb9850381c399b00dfe897e81cf4755
Deadly mind <oussamaayari2014@gmail.com>
2023-11-20 02:52:16 +0100

    add tags to search and filter lists

--- library/admin.py
+++ library/admin.py
@@ -7,8 +7,8 @@ from .models import Author, Book, Review
 
 class ProductAdmin(admin.ModelAdmin):       # Custom admin class for the Product model
     list_display = ['name', 'birth_date']   # Specifies the fields to display in the list view.
-    list_filter = ['name']                  # Specifies the fields to provide filtering options for.
-    search_fields = ['name']                # Enables search functionality on the 'name' field
+    list_filter = ['name', 'tags']                  # Specifies the fields to provide filtering options for.
+    search_fields = ['name', 'tags']                # Enables search functionality on the 'name' field
 
 
 
